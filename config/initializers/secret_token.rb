e0704e21c42eb44fffb07364383abfe7ea00ea92
coffewake@gmail.com <coffeitunes@coffeituness-MacBook-Pro.local>
2016-01-22 01:24:26 +0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo2rails::Application.config.secret_key_base = '8baf4ff2c8fa97e0d87351d99ef4124947b34eeef8f1caff4c06920a43a31376b1976cec3ceede0581690cda9b27a545e32e2149b1bfc5fd76f677e391d43409'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Demo2rails::Application.config.session_store :cookie_store, key: '_demo2rails_session'
